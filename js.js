28f07d8f5f206acd9a95c5db6f70ed3ff5b2ba98
Rhine <zrhinne@gmail.com>
2022-10-31 22:31:19 +0100

    add new js code

--- js.js
+++ js.js
@@ -34,13 +34,63 @@ document.wirte(phrase.substring(0,5));
 
 // 3-Boolean ... true or false
 
-// 4-Object
 
-// 5-Array ... Object
+// 4-Array ... Object 阵列
+var scores = [80, 60, 50, 70, 90];
+var friends = ["Tom", "Lily", "Rob"];
+var boolean = [true, false];
+scores[0] = 100;//改写第零位成绩
+document.wirte(scores[0]);
+document.write(friends.length);
 
-// 6-Function
 
-//变数
+// 5-Function 函式
+function hello(nam, age){
+    //document.write("hello" + nam + "你今年" + age);
+    return "hello" + nam + "你今年" + age;
+}
+hello();//对函式做呼叫
+hello("Lily", "82");
+
+function add(num1, num2){
+    return num1 + num2;
+    ducument.write("hello");//上面的return会覆盖这里的function
+}
+document.write (add(3, 9));
+
+
+// 6-Object 物件（可以存放很多key 和 value）
+var person = {
+    name:"Bob",
+    age:56,
+    is_male:true,
+    print_name:function(){
+        document.write(this.name);
+    }
+};
+person.print_name();//取得person这个object中的print_name key
+
+var movie = {
+    title:"刻在你心底的名字",
+    maker:"氧气电影",
+    duration:124,
+    actors:[
+        {
+            name:"陈浩森",
+            age:23,
+            is_male:true,
+        },
+        {
+            name:"增庆华",
+            age:24,
+            is_male:true,
+        }
+    ]
+}
+document.write(movie.actors[0].age);
+
+
+//变数 variable
 //只能是英文大小写或是数字或是$或是_的组合
 //开头不能是数字
 var my_name = "Lily";
@@ -54,4 +104,130 @@ if(userAge > 18){
 } else {
     console.log('get out')
 }
+
+//if 判断
+//1
+var hungry = true
+if(hungry){
+    document.write("eat");
+}
+//2
+var rainy = true
+if(rainy){
+    document.write("bus");
+}
+else{
+    document.write("bike");
+}
+//3
+var score = 100;
+if(score==100){
+    document.write("I'll give you 1000$");
+}
+else if(score>=80){
+    document.write("I'll give you 500$");
+}
+else if(score>=60){
+    document.write("I'll give you 100$");
+}
+else{
+    document.write("you own me 300$");
+};
+//4 且的类型&&
+var score = 100;
+var rainy = true;
+if(score==100 && rainy){
+    document.write("I'll give you 1000$");
+}
+else{
+    document.write("you own me 300$");
+}
+//5 或的类型 || (如果你考100或今天下雨，就给你1000)
+var score = 100;
+var rainy = true;
+if(score==100 || rainy){
+    document.write("I'll give you 1000$");
+}
+else{
+    document.write("you own me 300$");
+}
+//6 或的类型 || (如果你考100或今天没有（！）下雨，就给你1000)
+//!表示相反的意思
+var score = 100;
+var rainy = true;
+if(score==100 || !rainy){
+    document.write("I'll give you 1000$");
+}
+else{
+    document.write("you own me 300$");
+}
+
+//if and function 练习
+function max_num(num1, num2, num3){
+    if(num1>=num2 && num1>=num3){
+        return num1;
+    } 
+    else if(num2>=num1 && num2>=num3){
+        return num2;
+    }
+    else {
+        return num3;
+    }
+}
+document.write(max_num(2, 3, 5));
+
+
+//while 回圈 （会帮助我们重复执行一段代码，直到判断为false(这里i++等于i+1s)
+var i = 3;
+while(i<=3){
+    document.write(i);
+    document.write("<br/>");
+    i++;
+}
+//或者先执行再判断
+do{
+    document.write(i);
+    document.write("<br/>");
+    i++;
+}while(i<=3);
+
+
+//密码检验程式 (限制输入次数)if和while练习
+var password = 123456;
+var input;
+var entry_count = 0;
+var entry_limit = 3;
+var entry_of_limit = false;//判断现在有没有超出输入的次数
+
+while(password!=input && !out_of_limit){
+    entry_count++;
+    if(entry_count <= entry_limit){
+        input = prompt("Please enter your password");
+    }
+    else{
+        out_of_limit = true;
+    }
+}
+if (out_of_limit){
+    alert("out of entry limit times");
+}
+else{
+    alert("log in successful!");
+}
+
+//for 回圈（和while一样，可以帮我们重复执行一段代码，直到条件判断为false为止；但for更简洁
+var i = 0;
+while(i<10){
+    document.write(i);
+    document.write("<br/>");
+    i++;
+}
+//下面的for回圈等同于上面的while回圈（不一定是i也可以是q什么的
+for(var i=0; i<10; i++){
+    document.write(i);
+    document.write("<br/>");
+}
+
+//制作一个问答程式
+
 
